167781bf9d10cc03885aebee8d9f413a1af498d2
Bryan Lee <bryanmylee@fb.com>
2022-06-09 15:53:44 +0800

    feat: scroll progress indicator for accessibility

--- src/lib/components/Accessibility.svelte
+++ src/lib/components/Accessibility.svelte
@@ -3,6 +3,8 @@
 	import Seeing from '$lib/icons/Seeing.svelte';
 	import { isJsEnabled } from '$lib/utils/accessibility';
 
+	export let progress = 0;
+
 	const jsEnabled = isJsEnabled();
 </script>
 
@@ -11,6 +13,10 @@
 		? 'absolute inset-0'
 		: 'sticky top-0 w-screen h-screen'} flex flex-col justify-center items-center"
 >
+	<div
+		class="bg-white/10 w-full absolute top-1/2 -translate-y-1/2"
+		style:height="{progress * 100}vh"
+	/>
 	<Seeing color="#85D8FF" />
 	<Hearing color="#C46C66" />
 </div>
