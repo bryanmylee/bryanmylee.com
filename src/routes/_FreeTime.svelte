6e6c76aa675085dcab05deb97dbee4ce97f44414
Bryan Lee <bryanmylee@fb.com>
2022-06-10 09:21:06 +0800

    feat: out transition

--- src/routes/_FreeTime.svelte
+++ src/routes/_FreeTime.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { cubicOut } from 'svelte/easing';
+	import { cubicIn, cubicOut } from 'svelte/easing';
 	import ScrollProgress from '$lib/components/ScrollProgress.svelte';
 	import { isJsEnabled } from '$lib/utils/accessibility';
-	import { lerp } from '$lib/utils/math';
+	import { clamp, lerp } from '$lib/utils/math';
 
 	const jsEnabled = isJsEnabled();
-
-	let topProgress = 1;
-	$: t = $jsEnabled ? cubicOut(topProgress) : 1;
-	$: u = 1 - t;
 </script>
 
-<ScrollProgress scrollDistance={100} topInset={75} topPadding={125} bind:topProgress>
+<ScrollProgress scrollDistance={100} topInset={75} topPadding={125} let:topProgress let:outProgress>
+	{@const tIn = $jsEnabled ? cubicOut(topProgress) : 1}
+	{@const uIn = 1 - tIn}
+	{@const tOut = cubicIn(outProgress)}
+	{@const uOut = 1 - tOut}
 	<div
 		class="{$jsEnabled
 			? 'fixed'
@@ -21,7 +21,8 @@
 			role="group"
 			aria-labelledby="photos taken in my free time"
 			class="photos"
-			style="--t: {t}; --u: {u};"
+			style:opacity={clamp(1 - tOut * 2)}
+			style="--tIn:{tIn};--uIn:{uIn};--tOut:{tOut};--uOut:{uOut}"
 		>
 			<img src="/free-time/nature.webp" alt="a nature reserve" class="nature" />
 			<img src="/free-time/concert.webp" alt="a man in a concert" class="concert" />
@@ -32,8 +33,8 @@
 		</figure>
 		<h1
 			class="text-[8vw] font-bold text-center text-white leading-tight drop-shadow-2xl"
-			style:opacity={t}
-			style:transform="scale({lerp(0.95, 1, topProgress)})"
+			style:opacity={tIn * clamp(1 - tOut * 2)}
+			style:transform="scale({lerp(0.95, 1, tIn)}) translate(0, {tOut * -20}vh)"
 		>
 			And still manage<br />to make free time.
 		</h1>
@@ -53,37 +54,37 @@
 		}
 
 		& .nature {
-			transform: rotate(calc(-14deg + var(--u) * -30deg))
-				translate(calc(-30px - var(--u) * 80vw), calc(var(--u) * -80vh));
+			transform: rotate(calc(-14deg + var(--uIn) * -30deg))
+				translate(calc(-30px - var(--uIn) * 80vw), calc(var(--uIn) * -80vh));
 			@apply order-1;
 		}
 
 		& .concert {
-			transform: rotate(calc(7deg + var(--u) * 20deg)) translate(0, calc(var(--u) * -80vh));
+			transform: rotate(calc(7deg + var(--uIn) * 20deg)) translate(0, calc(var(--uIn) * -80vh));
 			@apply order-2;
 		}
 
 		& .climb {
-			transform: rotate(calc(-8deg + var(--u) * -20deg))
-				translate(calc(var(--u) * -100vw), calc(var(--u) * 20vh));
+			transform: rotate(calc(-8deg + var(--uIn) * -20deg))
+				translate(calc(var(--uIn) * -100vw), calc(var(--uIn) * 20vh));
 			@apply order-3;
 		}
 
 		& .dogs {
-			transform: rotate(calc(15deg + var(--u) * 20deg))
-				translate(calc(var(--u) * 80vw), calc(var(--u) * -40vh));
+			transform: rotate(calc(15deg + var(--uIn) * 20deg))
+				translate(calc(var(--uIn) * 80vw), calc(var(--uIn) * -40vh));
 			@apply order-4 md:order-2;
 		}
 
 		& .cafe {
-			transform: rotate(calc(-8deg + var(--u) * -20deg))
-				translate(calc(var(--u) * -80vw), calc(-30px + var(--u) * 80vh));
+			transform: rotate(calc(-8deg + var(--uIn) * -20deg))
+				translate(calc(var(--uIn) * -80vw), calc(-30px + var(--uIn) * 80vh));
 			@apply order-5;
 		}
 
 		& .ocean {
-			transform: rotate(calc(10deg + var(--u) * -20deg))
-				translate(calc(var(--u) * 80vw), calc(var(--u) * 80vh));
+			transform: rotate(calc(10deg + var(--uIn) * -20deg))
+				translate(calc(var(--uIn) * 80vw), calc(var(--uIn) * 80vh));
 			@apply order-6;
 		}
 	}
