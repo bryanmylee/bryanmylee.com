1e17f279bbbb2ffc8b893d6f6ee82a052159faaa
Bryan Lee <bryanmylee@fb.com>
2022-06-10 14:16:09 +0800

    fix: overlapping opacity

--- src/routes/_WorkWithYou.svelte
+++ src/routes/_WorkWithYou.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import ScrollProgress from '$lib/components/ScrollProgress.svelte';
+	import { isJsEnabled } from '$lib/utils/accessibility';
 	import { useWhite } from './_context';
 
 	const white = useWhite();
 	let topProgress = 0;
 	$: $white = topProgress;
+
+	const jsEnabled = isJsEnabled();
 </script>
 
 <ScrollProgress scrollDistance={125} topPadding={50} topInset={50} bind:topProgress>
-	<div class="sticky top-0 w-screen h-screen bg-gray-50 z-[-1]" style:opacity={topProgress}>
+	<div
+		class="sticky top-0 w-screen h-screen {$jsEnabled ? '' : 'bg-gray-50'} z-[-1]"
+		style:opacity={$jsEnabled ? topProgress : 1}
+	>
 		<div class="absolute inset-0 flex flex-col items-center justify-center gap-4">
 			<h1 class="font-bold leading-tight text-center text-dyn-6 drop-shadow-2xl" id="contact">
 				And I'd like to work with you.
