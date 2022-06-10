d4915da9aa1e6d4959df7ed8c763fde210ed8086
Bryan Lee <bryanmylee@fb.com>
2022-06-10 13:02:14 +0800

    tune: more scroll distance for work with

--- src/routes/_WorkWithYou.svelte
+++ src/routes/_WorkWithYou.svelte
@@ -8,7 +8,7 @@
 	$: $theme = topProgress <= 0 ? 'dark' : 'light';
 </script>
 
-<ScrollProgress scrollDistance={100} topPadding={50} topInset={50} bind:topProgress>
+<ScrollProgress scrollDistance={125} topPadding={50} topInset={50} bind:topProgress>
 	<div class="sticky top-0 w-screen h-screen bg-gray-50 z-[-1]" style:opacity={topProgress}>
 		<div class="absolute inset-0 flex flex-col items-center justify-center gap-4">
 			<h1 class="font-bold leading-tight text-center text-dyn-6 drop-shadow-2xl" id="contact">
