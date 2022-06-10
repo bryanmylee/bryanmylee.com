ffa5d41d717e438932ebcf054a59d303658f2b65
Bryan Lee <bryanmylee@fb.com>
2022-06-10 10:56:11 +0800

    feat: contact button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	interface $$Props extends svelte.JSX.HTMLAttributes<HTMLButtonElement> {
+		class?: string;
+		href?: string;
+	}
+
+	let _class = '';
+	export { _class as class };
+
+	export let href: string | undefined = undefined;
+	const tag = href === undefined ? 'button' : 'a';
+</script>
+
+<svelte:element
+	this={tag}
+	{...tag === 'button' ? {} : { href, target: '_blank', rel: 'noopener noreferrer' }}
+	class="{_class} button"
+	{...$$restProps}
+	on:click
+>
+	<slot />
+</svelte:element>
+
+<style lang="postcss">
+	.button {
+		@apply text-cyan-800 bg-cyan-200 p-4 font-medium rounded-lg;
+		@apply transition-colors;
+		&:hover {
+			@apply bg-cyan-100;
+		}
+		&:active {
+			@apply bg-cyan-200/30;
+		}
+	}
+</style>
--- src/routes/_WorkWithYou.svelte
+++ src/routes/_WorkWithYou.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	import ScrollProgress from '$lib/components/ScrollProgress.svelte';
 	import { isJsEnabled } from '$lib/utils/accessibility';
 
@@ -10,10 +11,11 @@
 		class="{$jsEnabled ? 'fixed' : 'sticky'} top-0 w-screen h-screen bg-white"
 		style:opacity={topProgress}
 	>
-		<div class="absolute inset-0 flex items-center justify-center">
+		<div class="absolute inset-0 flex items-center justify-center flex-col gap-4">
 			<h1 class="text-dyn-6 font-bold text-center leading-tight drop-shadow-2xl">
 				And I'd like to work with you.
 			</h1>
+			<Button href="mailto:bryanmylee@gmail.com">bryanmylee@gmail.com</Button>
 		</div>
 	</div>
 </ScrollProgress>
