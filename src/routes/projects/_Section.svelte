c84f3b2f8de172a4a6584156f9bf5420eb6ea4f1
Bryan Lee <bryanmylee@gmail.com>
2022-06-11 16:48:00 +0800

    tune: header margin

--- src/routes/projects/_Section.svelte
+++ src/routes/projects/_Section.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="p-4 mx-auto layout-container">
-	<h1 id={title.replaceAll(/\s+/g, '-').toLowerCase()} class="mb-4 ml-3 text-3xl font-bold">
+	<h1 id={title.replaceAll(/\s+/g, '-').toLowerCase()} class="mb-4 ml-3 mt-4 text-3xl font-bold">
 		{title}
 	</h1>
 	<div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4 items-start">
