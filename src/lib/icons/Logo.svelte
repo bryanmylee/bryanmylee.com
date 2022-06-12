0efe374e2cd7c817f4d755b7ab1833dc8c160299
Bryan Lee <bryanmylee@fb.com>
2022-06-13 01:15:12 +0800

    tune: logo svg

--- src/lib/icons/Logo.svelte
+++ src/lib/icons/Logo.svelte
@@ -1,9 +1,20 @@
 <svg viewBox="0 0 25 50" fill="none" xmlns="http://www.w3.org/2000/svg" {...$$restProps}>
-	<path d="M25 3.8147e-06L5.56441e-07 1.62913e-06L12.5 12.5L25 3.8147e-06Z" fill="currentColor" />
 	<path
 		fill-rule="evenodd"
 		clip-rule="evenodd"
-		d="M12.5 25L16.6667 16.6667L25 25L16.6667 33.3333L-3.8147e-06 50L12.5 25ZM12.5 12.5L25 3.8147e-06L16.6667 16.6667L12.5 12.5ZM8.33333 16.6667L5.56441e-07 1.62913e-06L-1.62913e-06 25L8.33333 16.6667Z"
+		d="M 12.5 25 L 16.6667 16.6667 L 25 25 L 16.6667 33.3333 L -0.0000038147 50 L 12.5 25 Z"
+		fill="currentColor"
+	/>
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M 8.33333 16.6667 L 5.564409999999999e-7 0.00000162913 L -0.00000162913 25 L 8.33333 16.6667 Z"
+		fill="currentColor"
+	/>
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M 0 0 L 25 0 L 16.667 16.667"
 		fill="currentColor"
 	/>
 </svg>
