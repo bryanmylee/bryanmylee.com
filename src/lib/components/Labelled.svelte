30d58f96a10d86fe611fab857d284d9b69371497
Bryan Lee <bryanmylee@fb.com>
2022-06-13 02:55:20 +0800

    feat: github and linkedin

---
+++ src/lib/components/Labelled.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let label: string;
+	export let as = 'a';
+</script>
+
+<svelte:element this={as} class="relative group" {...$$restProps}>
+	<slot />
+	<span>
+		{label}
+	</span>
+</svelte:element>
+
+<style lang="postcss">
+	span {
+		@apply z-10 rounded-lg bg-gray-100 p-2 text-xs font-semibold shadow;
+		@apply absolute bottom-full left-1/2 mb-2 -translate-x-1/2 scale-0;
+		@apply group-hover:block group-hover:scale-100;
+		@apply pointer-events-none whitespace-nowrap;
+		@apply transition-transform;
+	}
+</style>
--- src/routes/_WorkWithYou.svelte
+++ src/routes/_WorkWithYou.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
+	import LinkIcon from '$lib/components/Labelled.svelte';
 	import ScrollProgress from '$lib/components/ScrollProgress.svelte';
+	import Labelled from '$lib/components/Labelled.svelte';
+	import GitHub from '$lib/icons/GitHub.svelte';
 	import { isJsEnabled } from '$lib/utils/accessibility';
 	import { useWhite } from './_context';
+	import LinkedIn from '$lib/icons/LinkedIn.svelte';
 
 	const white = useWhite();
 	let topProgress = 0;
@@ -15,12 +19,32 @@
 	<div class="sticky top-0 w-screen h-screen" style:opacity={$jsEnabled ? topProgress : 1}>
 		<div class="absolute inset-0 {$jsEnabled ? '' : 'bg-gray-50'}" />
 		<div class="absolute inset-0 bottom-[45vh] flex flex-col items-center justify-end gap-8">
-			<h1 class="font-bold leading-tight text-center text-dyn-6 drop-shadow-2xl" id="contact">
+			<h1 id="contact" class="font-bold leading-tight text-center text-dyn-6 drop-shadow-2xl">
 				And I'd like to work with you.
 			</h1>
 			<Button href="mailto:bryanmylee@gmail.com" target="_blank" rel="noopener noreferrer">
 				bryanmylee@gmail.com
 			</Button>
+			<div class="flex gap-4 items-center">
+				<Labelled
+					label="GitHub"
+					as="a"
+					href="https://github.com/bryanmylee"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<GitHub class="wh-6 group-hover:scale-125 transition-transform" />
+				</Labelled>
+				<Labelled
+					label="LinkedIn"
+					as="a"
+					href="https://www.linkedin.com/in/bryan-lee-min-yuan/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<LinkedIn class="wh-6 group-hover:scale-125 transition-transform" />
+				</Labelled>
+			</div>
 		</div>
 	</div>
 </ScrollProgress>
