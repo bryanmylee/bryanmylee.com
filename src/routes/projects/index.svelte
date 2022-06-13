6cca9397b60f0599d94b470c73cd4b3b17abeec6
Bryan Lee <bryanmylee@fb.com>
2022-06-13 21:26:47 +0800

    feat: cs441

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -195,6 +195,17 @@
 				We implemented a reliable and robust microservice architecture to handle market trading
 				functionality, listing expirations, and user notifications.
 			</p>
+			<p>
+				Most notably, we developed a framework-agnostic implementation of the <a
+					href="https://docs.microsoft.com/en-us/azure/architecture/reference-architectures/saga/saga"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					saga pattern
+				</a>
+				we named the <strong>Saga Engine</strong> to orchestrate transactions and compensating actions
+				across multiple microservices.
+			</p>
 		</div>
 	</ProjectCard>
 
@@ -226,34 +237,10 @@
 				unavailable.
 			</p>
 			<p>
-				We continuously check the health of external services. When an external service fails a
-				healthcheck, we queue all client requests and notify users of the temporary disruption. Once
-				the external service is healthy, we automatically trigger a recovery phase and continue with
-				regular execution.
-			</p>
-		</div>
-	</ProjectCard>
-
-	<ProjectCard
-		name="Optimizing hotel bookings"
-		description="CS421 Introduction to Machine Learning"
-		skills={['python', 'scikit']}
-		links={[
-			{
-				label: 'Data',
-				href: 'https://www.kaggle.com/jessemostipak/hotel-booking-demand',
-			},
-			{
-				label: 'Read our report',
-				href: '/projects/cs421-final-report.pdf',
-			},
-		]}
-	>
-		<div class="space-y-2">
-			<p>
-				We analyzed data on hotel booking demand and explored a variety of machine learning
-				techniques to predict the cancellation of a given hotel booking based on pre-existing
-				features.
+				To do so, we continuously check the health of external services. When an external service
+				fails a healthcheck, we queue all client requests and notify users of the temporary
+				disruption. Once the external service is healthy, we automatically trigger a recovery phase
+				and continue with regular execution.
 			</p>
 		</div>
 	</ProjectCard>
@@ -306,6 +293,66 @@
 			<p>For our efforts, we were awarded the <strong>Best Technical Team</strong> achievement.</p>
 		</div>
 	</ProjectCard>
+
+	<ProjectCard
+		name="Network and link layer emulation"
+		description="CS441 Network Security"
+		skills={['python']}
+		links={[
+			{
+				label: 'GitHub',
+				href: 'https://github.com/bryanmylee/smu-cs441-network-emulation',
+			},
+			{
+				label: 'Read our report',
+				href: '/projects/cs441-final-report.pdf',
+			},
+		]}
+	>
+		<div class="space-y-2">
+			<p>
+				We emulated a simplified version of the MAC and IP protocol on top of network sockets for
+				our physical layer.
+			</p>
+			<p>
+				With the emulated network, the project requires a study and implementation of IP spoofing,
+				IP filtering, and packet sniffing. Our group went beyond the requirements to implement
+				private and public network separation, a simple public routing protocol, the ARP protocol,
+				and IP traceback.
+			</p>
+		</div>
+	</ProjectCard>
+
+	<ProjectCard
+		name="Optimizing hotel bookings"
+		description="CS421 Introduction to Machine Learning"
+		skills={['python', 'scikit']}
+		links={[
+			{
+				label: 'Data',
+				href: 'https://www.kaggle.com/jessemostipak/hotel-booking-demand',
+			},
+			{
+				label: 'Read our report',
+				href: '/projects/cs421-final-report.pdf',
+			},
+		]}
+	>
+		<div class="space-y-2">
+			<p>
+				We analyzed data on hotel booking demand and explored a variety of machine learning
+				techniques to predict the cancellation of a given hotel booking based on pre-existing
+				features.
+			</p>
+		</div>
+	</ProjectCard>
 </Section>
 
 <div class="h-40 w-full bg-gray-50" />
+
+<style lang="postcss">
+	a {
+		@apply text-cyan-500 underline underline-offset-1;
+		@apply hover:text-cyan-400;
+	}
+</style>
