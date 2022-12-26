ad516032865819e10fac8be72e17865ece36c042
Bryan Lee <bryanmylee@gmail.com>
2022-12-26 11:37:32 +0800

    feat: blog index

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -7,6 +7,5 @@ export const load: PageServerLoad = async ({ params }) => {
 	const notionResponse = await notion.databases.query({
 		database_id: NOTION_DATABASE_ID,
 	});
-	console.log(notionResponse);
 	return notionResponse;
 };
