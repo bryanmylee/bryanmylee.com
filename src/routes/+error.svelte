5d35078274d4b7bbf35a41d609934df3215e8015
Bryan Lee <bryanmylee@gmail.com>
2022-12-29 00:47:25 +0800

    nit: missing page message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -13,7 +13,7 @@
 <section class="mx-auto p-4 h-[calc(100vh-7rem)] flex justify-center items-center">
 	<div class="space-y-4">
 		<p class="text-7xl font-mono font-semibold tabular-nums">{$page.status}</p>
-		<h1 class="text-xl">This is not the page you are looking for.</h1>
+		<h1 class="text-xl">There's nothing here!</h1>
 		<Button variant="primary" href="/#" class="inline-flex justify-center items-center gap-2">
 			Take me back <ArrowRight class="wh-5" />
 		</Button>
