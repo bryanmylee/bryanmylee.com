26e0b2a94ac2583f58a9c338871c22db1ba7f310
Bryan Lee <bryanmylee@gmail.com>
2022-12-29 00:44:49 +0800

    feat: error handling

---
+++ src/lib/utils/error.ts
@@ -0,0 +1,17 @@
+import { Ok, Err, Result } from 'oxide.ts';
+
+export const tryResult = <T>(fn: () => T): Result<T, unknown> => {
+	try {
+		return Ok(fn());
+	} catch (err) {
+		return Err(err);
+	}
+};
+
+export const asyncTryResult = async <T>(fn: () => Promise<T>): Promise<Result<T, unknown>> => {
+	try {
+		return Ok(await fn());
+	} catch (err) {
+		return Err(err);
+	}
+};
