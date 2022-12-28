6c9436cc5ac88cd8da77041cf269dfbc4dbb1f69
Bryan Lee <bryanmylee@gmail.com>
2022-12-29 00:46:47 +0800

    feat: main error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$lib/components/Button.svelte';
+	import ArrowRight from '$lib/icons/ArrowRight.svelte';
+	import { syncBackgroundWhite } from '$lib/utils/background';
+	import { useWhite } from './context';
+
+	const white = useWhite();
+	white.set(1);
+	syncBackgroundWhite(white);
+</script>
+
+<section class="mx-auto p-4 h-[calc(100vh-7rem)] flex justify-center items-center">
+	<div class="space-y-4">
+		<p class="text-7xl font-mono font-semibold tabular-nums">{$page.status}</p>
+		<h1 class="text-xl">This is not the page you are looking for.</h1>
+		<Button variant="primary" href="/#" class="inline-flex justify-center items-center gap-2">
+			Take me back <ArrowRight class="wh-5" />
+		</Button>
+	</div>
+</section>
