275a573b4fc5d13c841598f482670bbb94899bfd
Bryan Lee <bryanmylee@gmail.com>
2022-12-29 00:44:59 +0800

    feat: blog post error page

---
+++ src/routes/blog/[slug]/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Button from '$lib/components/Button.svelte';
+	import ArrowRight from '$lib/icons/ArrowRight.svelte';
+</script>
+
+<section class="mx-auto p-4 h-[calc(100vh-7rem)] flex justify-center items-center">
+	<div class="space-y-4">
+		<p class="text-7xl font-mono font-semibold tabular-nums">{$page.status}</p>
+		<h1 class="text-xl">This is not the page you are looking for.</h1>
+		<Button variant="primary" href="/blog" class="inline-flex justify-center items-center gap-2">
+			Find another <ArrowRight class="wh-5" />
+		</Button>
+	</div>
+</section>
--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,25 +1,50 @@
 import { NOTION_API_KEY } from '$env/static/private';
-import { Client } from '@notionhq/client';
+import { APIErrorCode, Client, isNotionClientError } from '@notionhq/client';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { metadataFromProperties } from '../metadataFromProperties';
+import { asyncTryResult } from '$lib/utils/error';
 
 export const load: PageServerLoad = async ({ params }) => {
+	const notion = new Client({ auth: NOTION_API_KEY });
 	const tokens = params.slug.split('-');
 	const id = tokens[tokens.length - 1];
-	const notion = new Client({ auth: NOTION_API_KEY });
-	const [page, pageBlocks] = await Promise.all([
-		notion.pages.retrieve({ page_id: id }),
-		notion.blocks.children.list({
-			block_id: id,
-		}),
-	]);
+
+	const data = await asyncTryResult(() => {
+		return Promise.all([
+			notion.pages.retrieve({ page_id: id }),
+			notion.blocks.children.list({
+				block_id: id,
+			}),
+		]);
+	});
+
+	if (data.isErr()) {
+		const err = data.unwrapErr();
+		if (isNotionClientError(err)) {
+			switch (err.code) {
+				case APIErrorCode.ValidationError:
+					throw error(400, err.message);
+				case APIErrorCode.ObjectNotFound:
+					throw error(404, err.message);
+				default:
+					throw error(500, err.message);
+			}
+		} else if (err instanceof Error) {
+			throw error(500, err.message);
+		}
+	}
+
+	const [page, pageBlocks] = data.unwrap();
+
 	if (!('properties' in page)) {
 		throw error(500, {
 			message: 'Could not load page properties',
 		});
 	}
+
 	const { title, subtitle, formattedDate } = metadataFromProperties(page.properties);
+
 	return {
 		title,
 		subtitle,
