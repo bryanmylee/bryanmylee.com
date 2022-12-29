ec378f65301001546f6705641e15fb76f90c03a2
Bryan Lee <bryanmylee@gmail.com>
2022-12-29 18:13:45 +0800

    feat: load all blocks

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -4,6 +4,11 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { metadataFromProperties } from '../metadataFromProperties';
 import { asyncTryResult } from '$lib/utils/error';
+import type {
+	BlockObjectResponse,
+	ListBlockChildrenResponse,
+	PartialBlockObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const notion = new Client({ auth: NOTION_API_KEY });
@@ -11,12 +16,7 @@ export const load: PageServerLoad = async ({ params }) => {
 	const id = tokens[tokens.length - 1];
 
 	const data = await asyncTryResult(() => {
-		return Promise.all([
-			notion.pages.retrieve({ page_id: id }),
-			notion.blocks.children.list({
-				block_id: id,
-			}),
-		]);
+		return Promise.all([notion.pages.retrieve({ page_id: id }), loadAllBlockResults(notion, id)]);
 	});
 
 	if (data.isErr()) {
@@ -35,7 +35,7 @@ export const load: PageServerLoad = async ({ params }) => {
 		}
 	}
 
-	const [page, pageBlocks] = data.unwrap();
+	const [page, content] = data.unwrap();
 
 	if (!('properties' in page)) {
 		throw error(500, {
@@ -49,6 +49,22 @@ export const load: PageServerLoad = async ({ params }) => {
 		title,
 		subtitle,
 		formattedDate,
-		content: pageBlocks.results,
+		content,
 	};
+};
+
+const loadAllBlockResults = async (notion: Client, blockId: string) => {
+	const results: (PartialBlockObjectResponse | BlockObjectResponse)[] = [];
+	let startCursor: string | undefined = undefined;
+	let hasMore = true;
+	while (hasMore) {
+		const block: ListBlockChildrenResponse = await notion.blocks.children.list({
+			block_id: blockId,
+			start_cursor: startCursor,
+		});
+		results.push(...block.results);
+		startCursor = block.next_cursor ?? undefined;
+		hasMore = block.has_more;
+	}
+	return results;
 };
