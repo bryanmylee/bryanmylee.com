fb6959e628b8cf74fb8c5bbf52db1a006d730ac0
Bryan Lee <bryanmylee@gmail.com>
2024-08-28 03:13:06 +0800

    chore: update work experience

--- src/lib/work.ts
+++ src/lib/work.ts
@@ -5,6 +5,7 @@ export interface Work {
 	title: string;
 	details: string[];
 	homepage?: string;
+	wideLayout?: boolean;
 }
 
 export const WORK_EXPERIENCE: Work[] = [
@@ -14,12 +15,14 @@ export const WORK_EXPERIENCE: Work[] = [
 		to: 'present',
 		title: 'Software Engineer',
 		details: [
-			'Developed a flexible and cohesive design system to ensure consistency in our application interface',
-			"Optimized the system's data model for efficient and accurate data synchronization across views",
-			'Built multiple high-performance data-driven views from scratch, including Gantt charts and systems diagramming tools with advanced functionality and smooth interactions',
-			'Collaborated with product, UX, and engineering teams to design and refine feature requirements',
+			"Built a client-side application data layer for instant read/write performance, automated tracking and committing of local changes to the server, and reactive updates that drive UI efficiently with support for highly relational data. This standardized the most common data access patterns in our codebase and enabled the development of significantly more powerful features including version control",
+			'Designed and built a powerful diagramming tool for model-based systems engineering (MBSE) that supports customizable interface edge paths and an arbitrary number of nested sub-flows of systems to support our system- of-systems systems engineering paradigm',
+			"Set up E2E testing within CI pipelines using Playwright and Webdriver to reduce regressions in functionality",
+			'Led and designed a data migration from MongoDB to PostgreSQL to enable highly connected data models',
+			'Implemented a library of flexible and accessible React UI components aligned with our design system, improving the consistency of our UI and reducing wasted effort between our design and engineering team',
 		],
 		homepage: 'https://charter.space/',
+		wideLayout: true,
 	},
 	{
 		companyName: 'Meta',
@@ -27,10 +30,10 @@ export const WORK_EXPERIENCE: Work[] = [
 		to: 'Aug 2022',
 		title: 'Enterprise Engineer',
 		details: [
-			'Designed a database for logging critical transactions across all Meta Enterprise applications',
-			'Managed a diverse set of requirements from stakeholders across internal teams and Enterprise clients',
-			'Built a pagination and data-fetching solution for first-class interactivity and responsiveness in web clients',
-			'Extended administrative tools to manage and explore thousands of critical transaction logs efficiently',
+			'Designed a universal database for logging critical user actions across all Meta Enterprise applications that now provides traceability of user behaviour on Enterprise applications for audit and debugging',
+			'Built a pagination and data-fetching solution for first-class interactivity and responsiveness in web clients, overcoming existing database limitations and meet functional requirements on a tight deadline',
+			'Extended our administrative tools, enabling the management and exploration of critical action logs efficiently',
+			'Fixed issues found in internal tools owned by remote teams, improving the developer experience for all engineers',
 		],
 		homepage: 'https://meta.com/',
 	},
@@ -50,7 +53,7 @@ export const WORK_EXPERIENCE: Work[] = [
 		companyName: 'DSO National Laboratories',
 		from: 'May 2021',
 		to: 'Jul 2021',
-		title: 'Cybersecurity Research Intern',
+		title: 'Cybersecurity Researcher',
 		details: [
 			'Researched a wide variety of JavaScript fuzzing technologies, from American Fuzzy Lop to DIE',
 			'Explored the effectiveness and efficiency of ensemble fuzzing for vulnerability detection in JavaScript engines',
@@ -62,7 +65,7 @@ export const WORK_EXPERIENCE: Work[] = [
 		companyName: 'Garuda Robotics',
 		from: 'May 2020',
 		to: 'Aug 2020',
-		title: 'Engineering Intern',
+		title: 'Software Engineer',
 		details: [
 			'Developed warning system for beyond-visual-line-of-sight drone flight system',
 			'Spearheaded restructuring of a large React application on Redux for project maintainability',
