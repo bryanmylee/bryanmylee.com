859b4a2b800158a965f326df66e6e2e1c3502625
Bryan Lee <bryanmylee@gmail.com>
2024-09-03 20:35:56 +0800

    fix: error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -11,11 +11,13 @@
 	syncBgInkRatio(isDark, bgInkRatio);
 </script>
 
-<section class="mx-auto flex h-[calc(100vh-7rem)] items-center justify-center p-4">
+<section
+	class="mx-auto flex h-[calc(100vh-7rem)] w-[65ch] flex-col items-start justify-center p-4 text-ink"
+>
 	<div class="flex flex-col gap-4">
 		<p class="font-mono text-7xl font-semibold tabular-nums">{$page.status}</p>
 		<h1 class="text-xl">There's nothing here!</h1>
-		<Button variant="primary" href="/#" class="inline-flex items-center justify-center gap-2">
+		<Button variant="primary" href="/#" class="inline-flex w-fit items-center justify-center gap-2">
 			Take me back <ArrowRight class="wh-5" />
 		</Button>
 	</div>
--- src/routes/blog/[slug]/+error.svelte
+++ src/routes/blog/[slug]/+error.svelte
@@ -4,11 +4,15 @@
 	import ArrowRight from '$lib/icons/ArrowRight.svelte';
 </script>
 
-<section class="mx-auto flex h-[calc(100vh-7rem)] items-center justify-center p-4">
+<section class="mx-auto flex h-[calc(100vh-7rem)] w-[65ch] items-center p-4 text-ink">
 	<div class="flex flex-col gap-4">
 		<p class="font-mono text-7xl font-semibold tabular-nums">{$page.status}</p>
 		<h1 class="text-xl">This is not the page you are looking for.</h1>
-		<Button variant="primary" href="/blog" class="inline-flex items-center justify-center gap-2">
+		<Button
+			variant="primary"
+			href="/blog"
+			class="inline-flex w-fit items-center justify-center gap-2"
+		>
 			Find another <ArrowRight class="wh-5" />
 		</Button>
 	</div>
