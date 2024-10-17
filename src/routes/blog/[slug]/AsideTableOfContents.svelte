372bfe5748e4710efc0d84cba3a6c06000e66c1c
Bryan Lee <bryanmylee@gmail.com>
2024-10-17 14:13:06 +0800

    fix: indentation of toc items

--- src/routes/blog/[slug]/AsideTableOfContents.svelte
+++ src/routes/blog/[slug]/AsideTableOfContents.svelte
@@ -19,7 +19,8 @@
 				<ul class="flex flex-col p-2">
 					{#each headings as heading}
 						{@const richTextArray = heading.heading.rich_text}
-						<li>
+						{@const depth = heading.depth}
+						<li class={depth === 1 ? 'ml-0' : depth === 2 ? 'ml-4' : 'ml-8'}>
 							<a
 								href="#{getHeadingId(getPlainText(richTextArray))}"
 								class="block rounded-lg px-2 py-1.5 text-xs text-ink hover:bg-gray-400/10"
