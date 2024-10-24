566e50c6dcbe61d1808a8f2bf43ccfede37c7aba
Bryan Lee <bryanmylee@gmail.com>
2024-10-24 22:24:16 +0800

    feat: patch reading of animated images

--- src/routes/projects/ExampleImage.svelte
+++ src/routes/projects/ExampleImage.svelte
@@ -6,25 +6,11 @@
 	export let alt: string;
 	let className = '';
 	export { className as class };
-
-	$: imageModule = PROJECT_CONTENT_IMAGES[src];
-	$: imageClass = twMerge('rounded-xl shadow-md ring-1 ring-ink-200', className);
 </script>
 
-<!-- workaround for lack of animated gif support -->
-{#if src.endsWith('.gif')}
-	<img
-		src="src/routes/projects/content/{src}"
-		{alt}
-		width={imageModule.default.img.w}
-		height={imageModule.default.img.h}
-		class={imageClass}
-	/>
-{:else}
-	<enhanced:img
-		src={imageModule.default}
-		sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
-		{alt}
-		class={imageClass}
-	/>
-{/if}
+<enhanced:img
+	src={PROJECT_CONTENT_IMAGES[src].default}
+	{alt}
+	sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+	class={twMerge('rounded-xl shadow-md ring-1 ring-ink-200', className)}
+/>
